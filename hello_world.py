f18578a2548a513867801a42ec468ffc3793d594
runda-git <84775206+runda-git@users.noreply.github.com>
2021-06-07 19:52:17 +0800

    Create hello_world.py
    intro to variables

---
+++ hello_world.py
@@ -0,0 +1,28 @@
+print("Hello, Anika!")
+
+day = "Monday"
+month = "June"
+print(f"Today is: {day} and the month is:{month}")
+# print (day)
+# print (month)
+# this is a comment / notes
+
+
+run1_dist = 1400
+print(run1_dist)
+print(type(run1_dist))
+run2_dist = 1800
+
+total_distance = run1_dist + run2_dist
+difference = run2_dist - run1_dist
+print(total_distance)
+print(difference)
+
+
+
+run3_dist = 1.7
+run4_dist = 1.35
+print(type(run4_dist))
+
+print(run1_dist / 1000)
+print(run4_dist * 1000)
